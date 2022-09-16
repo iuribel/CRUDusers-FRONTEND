1fcea308b58c58a2815481794318acaf8b37c750
Nathalia Uribe <iuribel@unal.edu.co>
2022-09-16 16:22:55 -0500

    Edit final commit

--- src/components/Edituser.js
+++ src/components/Edituser.js
@@ -1,37 +1,61 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
 
 const Edituser = ({user})=>{
-    console.log(user);
+    const [Nombre, setNombre] = useState(user.nombre);
+    const [Birthday, setBirthday] = useState(user.birthday);
+    const [Id, setId] = useState(user.id);
+    //Edit user (update user)
+    const updateUser = async e =>{
+        e.preventDefault();
+        try {
+            const body = {Nombre,Birthday,Id};
+            const response = await fetch(`http://localhost:5000/users/${user.id}`,{
+                method:"PUT",
+                headers:{"Content-Type":"application/json"},
+                body: JSON.stringify(body)
+            });
+            console.log(response);
+            window.location.reload();
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
     return (
     <Fragment>
        
-        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#myModal">
+        <button type="button" className="btn btn-warning" data-toggle="modal" data-target={`#id${user.id}`}>
         Editar
         </button>
 
 
-        <div class="modal" id="myModal">
-        <div class="modal-dialog">
-            <div class="modal-content">
+        <div className="modal" id={`id${user.id}`}>
+        <div className="modal-dialog">
+            <div className="modal-content">
 
             
-            <div class="modal-header">
-                <h4 class="modal-title">Editar usuario</h4>
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <div className="modal-header">
+                <h4 className="modal-title">Editar usuario</h4>
+                <button type="button" className="close" data-dismiss="modal">&times;</button>
             </div>
 
             
-            <div class="modal-body">
-                <input type="text" className="form-control"/>
-                <input type="text" className="form-control" />
-                <input type="number" className="form-control" />
+            <div className="modal-body">
+                <input type="text" className="form-control" value={Nombre} onChange={e =>
+                setNombre(e.target.value)}/>
+                <input type="text" className="form-control" value = {Birthday} onChange={e =>
+                setBirthday(e.target.value)}/>
+                <input type="number" className="form-control" value ={Id} onChange={e =>
+                setId(e.target.value)}/>
             </div>
 
             
-            <div class="modal-footer">
+            <div className="modal-footer">
 
-                <button type="button" class="btn btn-warning" data-dismiss="modal">Guardar</button>
-                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                <button type="button" className="btn btn-warning" data-dismiss="modal"
+                onClick={e => updateUser(e)}
+                >Guardar</button>
+                <button type="button" className="btn btn-danger" data-dismiss="modal">Cancelar</button>
             </div>
 
             </div>
